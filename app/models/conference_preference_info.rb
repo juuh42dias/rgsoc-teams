7ea38474ba93bfefb10e9c45f9edfb62a088db69
Juliana Dias <juuh42dias@gmail.com>
2017-07-27 17:04:47 -0300

    adding conference_preference_info model

---
+++ app/models/conference_preference_info.rb
@@ -0,0 +1,15 @@
+class ConferencePreferenceInfo < ApplicationRecord
+  belongs_to :team
+  has_many :conference_preferences
+
+  accepts_nested_attributes_for :conference_preferences, allow_destroy: true#, reject_if: :without_conferences?
+
+  def with_preferences_build
+    conference_preferences.build(option: 1) unless conference_preferences.find_by(option: 1).present?
+    conference_preferences.build(option: 2) unless conference_preferences.find_by(option: 2).present?
+  end
+
+  def without_conferences?(att)
+    att[:conference_id].blank?
+  end
+end
