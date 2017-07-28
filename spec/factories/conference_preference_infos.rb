973e96b2102fc8863c8faaea0ece27345dd29eb2
Juliana Dias <juuh42dias@gmail.com>
2017-07-28 13:39:25 -0300

    Minor adjustments ading tests

--- spec/factories/conference_preference_infos.rb
+++ spec/factories/conference_preference_infos.rb
@@ -3,15 +3,17 @@ FactoryGirl.define do
     condition_term_ticket true
     condition_term_cost true
     lightning_talk true
-    comment  { FFaker::Lorem.paragraph }
+    comment { FFaker::Lorem.paragraph }
     team { FactoryGirl.create(:team, :in_current_season) }
 
     trait :with_preferences do
       after(:create) do |cp_info|
         conf1 = FactoryGirl.create(:conference, :in_current_season)
         conf2 = FactoryGirl.create(:conference, :in_current_season)
-        cp_info.conference_preferences.create(conference_id: conf1.id, option: 1)
-        cp_info.conference_preferences.create(conference_id: conf2.id, option:2)
+        cp_info.conference_preferences
+               .create(conference_id: conf1.id, option: 1)
+        cp_info.conference_preferences
+               .create(conference_id: conf2.id, option: 2)
       end
     end
   end
--- spec/models/conference_preference_info_spec.rb
+++ spec/models/conference_preference_info_spec.rb
@@ -1,6 +1,14 @@
 require 'spec_helper'
 
 describe ConferencePreferenceInfo, type: :model do
-    it { is_expected.to belongs_to(:team) }
-    it { is_expected.to have_many(:conference_preferences) }
+  it { is_expected.to belong_to(:team) }
+  it { is_expected.to have_many(:conference_preferences) }
+
+  describe '#with_preferences_build' do
+    let(:conference_preference_info) { FactoryGirl.create(:conference_preference_info) }
+
+    it 'should build a conference_preferences' do
+      expect(conference_preference_info.with_preferences_build).to be true
+    end
+  end 
 end
