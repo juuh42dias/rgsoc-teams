55e4426ea542c22becd5c172515b53669caf1f18
Juliana Dias <juuh42dias@gmail.com>
2017-07-28 12:54:07 -0300

    Adding minor adjustments

--- app/models/conference_preference.rb
+++ app/models/conference_preference.rb
@@ -1,8 +1,7 @@
+# frozen_sring_literal: true
+
+# nodoc
 class ConferencePreference < ActiveRecord::Base
   belongs_to :conference_preference_info
   belongs_to :conference
-
-  def without_conferences?(att)
-    att[:conference_id].blank?
-  end
 end
--- app/models/conference_preference_info.rb
+++ app/models/conference_preference_info.rb
@@ -1,8 +1,17 @@
+# frozen_literal_string: true
+
+# nodoc
 class ConferencePreferenceInfo < ApplicationRecord
+  # validates :conference_id, presence: true
+  # validates :condition_term_ticket presence: true
+  # validates :condition_term_cost presence: true
+  # before_save :accepted_conditions?
+
   belongs_to :team
   has_many :conference_preferences
-
-  accepts_nested_attributes_for :conference_preferences, allow_destroy: true#, reject_if: :without_conferences?
+  accepts_nested_attributes_for :conference_preferences,
+                                allow_destroy: true,
+                                reject_if: :without_conferences?
 
   def with_preferences_build
     conference_preferences.build(option: 1) unless conference_preferences.find_by(option: 1)
@@ -12,4 +21,9 @@ class ConferencePreferenceInfo < ApplicationRecord
   def without_conferences?(att)
     att[:conference_id].blank?
   end
+
+  # def accepted_conditions?
+  #   return false if condition_term_ticket.blank? && condition_term_cost.blank?
+  #   errors.add(:base, 'Accept terms and conditions is necessary')
+  # end
 end
